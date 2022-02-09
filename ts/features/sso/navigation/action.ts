af3039d081d682d51ee71e8817023ae2399ab5f3
Francesco Persico <francescopersico@gmail.com>
2022-02-09 22:47:01 +0100

    Improve UI for SSOWebviewScreen

--- ts/features/sso/navigation/action.ts
+++ ts/features/sso/navigation/action.ts
@@ -1,9 +1,15 @@
 import { NavigationActions } from "react-navigation";
 
 import NavigationService from "../../../navigation/NavigationService";
+import { resetInternalRouteNavigation } from "../../../store/actions/internalRouteNavigation";
 import { InferNavigationParams } from "../../../types/react";
 import SSOWebviewScreen from "../screens/SSOWebviewScreen";
 import SSO_ROUTES from "./routes";
+
+export const goBack = (dispatch: Dispatch) => {
+  dispatch(resetInternalRouteNavigation());
+  NavigationService.dispatchNavigationAction(NavigationActions.back());
+};
 
 export const navigateToSSOLogin = () =>
   NavigationService.dispatchNavigationAction(
--- ts/features/sso/screens/SSOWebviewScreen.tsx
+++ ts/features/sso/screens/SSOWebviewScreen.tsx
@@ -1,6 +1,6 @@
-import { Content } from "native-base";
+import { View } from "native-base";
 import * as React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import WebView from "react-native-webview";
 import { NavigationInjectedProps } from "react-navigation";
 import { connect } from "react-redux";
@@ -8,6 +8,7 @@ import { connect } from "react-redux";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
 import { Dispatch } from "../../../store/actions/types";
 import { GlobalState } from "../../../store/reducers/types";
+import { goBack } from "../navigation/action";
 
 type NavigationParams = Readonly<{
   uri: string;
@@ -20,23 +21,28 @@ type Props = OwnProps &
   ReturnType<typeof mapDispatchToProps>;
 
 const styles = StyleSheet.create({
-  flex: { flex: 1 }
+  webViewWrapper: { flex: 1 }
 });
 
 const SSOWebviewScreen: React.FunctionComponent<Props> = (props: Props) => {
   const uri = props.navigation.getParam("uri");
-  
+
   // TODO: Add other parameters to WebView component
   return (
-    <BaseScreenComponent headerTitle="SSO">
-        <Content contentContainerStyle={styles.flex}>
-          <WebView source={{ uri }} />
-        </Content>
+    <BaseScreenComponent
+      headerTitle="External site"
+      goBack={props.handleGoBack}
+    >
+      <View style={styles.webViewWrapper}>
+        <WebView source={{ uri }} />
+      </View>
     </BaseScreenComponent>
   );
 };
 const mapStateToProps = (state: GlobalState) => ({});
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  handleGoBack: () => goBack(dispatch)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SSOWebviewScreen);
